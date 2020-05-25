57379d0faf03c1fa7f0ff681ae16f5f27abb0d66
bitcher8 <nikita.saakyan@gmail.com>
2020-05-25 12:25:22 +0300

    get contact by id

--- app/src/main/java/com/example/addressbook/AddressBookDb.java
+++ app/src/main/java/com/example/addressbook/AddressBookDb.java
@@ -26,28 +26,28 @@ public class AddressBookDb extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT," +
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT," +
                 "Phone TEXT, Email TEXT, Street TEXT, City TEXT, State TEXT, Zip TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 
-    public boolean insertData(String name,String phone,String email, String street, String city, String state, String zip) {
+    public boolean insertData(String name, String phone, String email, String street, String city, String state, String zip) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2,name);
-        contentValues.put(COL_3,phone);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, phone);
         contentValues.put(COL_4, email);
         contentValues.put(COL_5, street);
         contentValues.put(COL_6, city);
         contentValues.put(COL_7, state);
         contentValues.put(COL_8, zip);
-        long result = db.insert(TABLE_NAME,null ,contentValues);
-        if(result == -1)
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1)
             return false;
         else
             return true;
@@ -55,31 +55,40 @@ public class AddressBookDb extends SQLiteOpenHelper {
 
     public Cursor getAllData() {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("select * from "+TABLE_NAME,null);
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
         return res;
     }
 
-    public Integer deleteData (String id) {
+    public Cursor getById(String id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
+        Cursor result = db.rawQuery("select * from " + TABLE_NAME +" where id =?"
+                , new String[] { id + " " });
+
+        return  result;
+
+    }
+
+    public Integer deleteData(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[]{id});
     }
 
     public boolean updateData(String id, String name, String phone, String email, String street, String city, String state, String zip) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2,name);
-        contentValues.put(COL_3,phone);
-        contentValues.put(COL_4,email);
-        contentValues.put(COL_5,street);
-        contentValues.put(COL_6,city);
-        contentValues.put(COL_7,state);
-        contentValues.put(COL_8,zip);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, phone);
+        contentValues.put(COL_4, email);
+        contentValues.put(COL_5, street);
+        contentValues.put(COL_6, city);
+        contentValues.put(COL_7, state);
+        contentValues.put(COL_8, zip);
 
-        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        db.update(TABLE_NAME, contentValues, "ID = ?", new String[]{id});
         return true;
     }
 
-    public Cursor getSortedByName(){
+    public Cursor getSortedByName() {
         SQLiteDatabase db = this.getReadableDatabase();
 
         String[] projection = {
